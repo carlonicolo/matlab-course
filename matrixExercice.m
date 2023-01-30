bc05d295155cf1967fd379188115b0701d136804
Carlo <carlo.nicolo@hotmail.com>
2023-01-30 01:10:46 +0100

    Matrix exercice

---
+++ matrixExercice.m
@@ -0,0 +1,29 @@
+% Compute max value, min value, medium value of matrix A and vector v
+A=[8 2 -3; 3 -1 0; 6 0 1]
+v=[-2 4 5]
+
+% Min v
+disp("Min v: ")
+fprintf("%i\n\n",min(v))
+
+% Max v
+disp("Max v: ")
+fprintf("%i\n\n",max(v))
+
+% Mean v
+disp("Mean v: ")
+fprintf("%i\n\n",mean(v))
+
+disp("------- Matrix -------")
+% Min v
+disp("Min A: ")
+fprintf("%i\n\n",min(A))
+
+% Max v
+disp("Max A: ")
+fprintf("%i\n\n",max(A))
+
+% Mean v
+disp("Mean A: ")
+fprintf("%i\n\n",mean(A))
+
