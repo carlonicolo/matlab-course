966c3d104bce937c099f1625d678163676adbc3d
Carlo <carlo.nicolo@hotmail.com>
2023-01-30 01:39:08 +0100

    Text and char

---
+++ textVariables.m
@@ -0,0 +1,19 @@
+text="This is a text variable"
+
+class(text)
+
+vector=["This" "is" "a" "vector"]
+
+vector(4)
+
+matrix=["This" "is"; "a" "matrix"; "of" "words"]
+
+% Variable char
+text="This is a text variable"
+t1=char(text)
+class(t1)
+
+t2='We are using Matlab'
+class(t2)
+
+[t1 ' . ' t2]
---
+++ variables.m
@@ -0,0 +1,5 @@
+e=2.71828
+g=9.81
+phi=(1+sqrt(5))/2
+
+save('MathConstants','e','g','phi')
